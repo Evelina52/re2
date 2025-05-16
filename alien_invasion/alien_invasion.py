50312a572cd7e9525f616c677fd64582608ae45c
Evelina52 <ettxsz@mail.ru>
2025-05-16 19:10:56 +0300

    commit1

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,27 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(ai_settings, screen)
+    bg_color = (230, 230, 230)
+    while True:
+        ship.update()
+        gf.check_events(ship)
+        gf.update_screen(ai_settings, screen, ship)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        pygame.display.flip()
+run_game()
